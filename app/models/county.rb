5c1e176bfbba0a567698bcf71c0ae9a7c778aa07
trevmckendrick <trevormckendrick@gmail.com>
2014-08-21 03:22:40 -0600

    Add site hash to county model (moved from seed)

--- app/models/county.rb
+++ app/models/county.rb
@@ -1,3 +1,35 @@
 class County < ActiveRecord::Base
   has_many :properties
+
+  ACCESSOR_SITE = {
+  "Beaver County" => "Not available online",
+  "Box Elder County" => "http://erecord.boxeldercounty.org/eaglesoftware/taxweb/search.jsp",
+  "Cache County" => "",
+  "Carbon County" => "",
+  "Daggett County" => "",
+  "Davis County" => "",
+  "Duchesne County" => "",
+  "Emery County" => "",
+  "Garfield County" => "",
+  "Grand County" => "",
+  "Iron County" => "",
+  "Juab County" => "",
+  "Kane County" => "",
+  "Millard County" => "",
+  "Morgan County" => "",
+  "Piute County" => "",
+  "Rich County" => "",
+  "Salt Lake County" => "http://assessor.slco.org/",
+  "San Juan County" => "",
+  "Sanpete County" => "",
+  "Sevier County" => "",
+  "Summit County" => "",
+  "Tooele County" => "",
+  "Uintah County" => "",
+  "Utah County" => "",
+  "Wasatch County" => "",
+  "Washington County" => "",
+  "Wayne County" => "",
+  "Weber County" => ""
+  }
 end
