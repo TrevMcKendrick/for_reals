38a4813ce8b1b511f79b580e63d73f2691097bad
trevmckendrick <trevormckendrick@gmail.com>
2014-08-21 02:28:16 -0600

    One test finally working :(

--- test/models/property_test.rb
+++ test/models/property_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
+require 'pp'
 
 class PropertyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "county_should_be_assigned" do
+    properties(:cedar_pine_court).assign_county
+    assert("Salt Lake County" == properties(:cedar_pine_court).county.name, "County wasn't assigned to property")
+  end
 end
